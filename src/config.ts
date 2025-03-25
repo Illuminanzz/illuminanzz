e8a7bb25acc51d8327120fd390073f68a7b29085
madhu <madhu@localhost>
2025-03-25 15:22:29 +0530

    change title name

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Illuminnazz';
+export const SITE_TITLE = 'Illuminazz';
 export const SITE_DESCRIPTION = 'Dance for Love,Life and Longevity';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
