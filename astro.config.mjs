fb97e8ab0996bd94243fc593f1bc7ee27cffed9c
Illuminanzz <veeanta7@gmail.com>
2025-03-29 18:51:06 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.veeanta.com",
+  site: "https://veeanta.com",
   integrations: [mdx(), sitemap(), tailwind()],
 
 });
